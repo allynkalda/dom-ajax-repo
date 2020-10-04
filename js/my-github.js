a7012cb095c36fabd209f358193bd64284e89d89
allyn <allyn.alda@gmail.com>
2020-10-04 15:18:59 +0200

    get github

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,31 @@
 // Write code here to communicate with Github
+
+// GET request for list of repositories
+// More info on how this API is constructed in https://developer.github.com/v3/repos/#list-repositories-for-a-user
+
+const getReposUrl = "https://api.github.com/users/joannaWebDev/repos";
+
+const fetchRepos = async () => {
+  const response = await fetch(getReposUrl);
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const repos = await response.json();
+  return repos;
+};
+
+fetchRepos()
+  .then(repos => {
+    console.log(repos);
+    repos.map(repo => {
+      $("#repos-list").append(`<li><a href=${repo.url}>${repo.name}</a></li>`);
+    });
+    $("#repos-count").html(`${repos.length}`);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+
